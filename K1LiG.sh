7e3b98318d9d6d5e0649444de8db160b5f126663
K1LiG_ <90832954+K1LiG@users.noreply.github.com>
2024-04-18 19:10:31 +0800

    Update K1LiG.sh

--- K1LiG.sh
+++ K1LiG.sh
@@ -40,7 +40,7 @@ do
               - $port:3000
             environment:
               - TZ=Asia/Shanghai
-              - SQL_DSN=\"root:feng32633@tcp(localhost:3306)/oneapi\"
+              - SQL_DSN=\"root:feng32633@tcp(root_mysql_1:3306)/oneapi\"
             volumes:
               - /home/ubuntu/data/one-api:/data
           mysql:
